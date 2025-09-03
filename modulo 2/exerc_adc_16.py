f771dec3da6455fd46d6e969291cb1fb5bef78af
BeiruthDEV <matheusbeiruth10@gmail.com>
2025-09-03 18:09:57 -0300

    Exercicios Adicionas Listas, Tuplas e Conjuntos

---
+++ modulo 2/exerc_adc_16.py
@@ -0,0 +1,12 @@
+"""
+Tupla Aninhada: Crie uma tupla de tuplas para representar uma matriz 2x2: matriz = ((1,
+2), (3, 4)). Acesse e imprima o valor 4.
+"""
+
+
+matriz = ((1, 2), (3, 4))
+
+
+valor = matriz[1][1]
+
+print(valor)
