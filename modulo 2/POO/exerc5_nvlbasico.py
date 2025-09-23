94c28b11d1391cf546dbd1ed3816fecc03a5f277
BeiruthDEV <matheusbeiruth10@gmail.com>
2025-09-23 20:59:27 -0300

    Exercicio 5 nivel basico

--- modulo 2/POO/exerc5_nvlbasico.py
+++ modulo 2/POO/exerc5_nvlbasico.py
@@ -1,15 +1,22 @@
 class ContaBancaria:
-    def __init__(self,titular,saldo_inicial):
+    def __init__(self, titular, saldo_inicial):
         self.titular = titular
         self.saldo = saldo_inicial
 
-
-
-    def depositar(self,valor):
+    def depositar(self, valor):
         if valor > 0:
             self.saldo += valor
-            print(f'Deposito de R${valor:.2f} realizado com sucesso')
+            print(f"Deposito de R${valor:.2f} realizado com sucesso")
 
         else:
             print("Valor de depósito inválido")
-        
+
+    def exibir_saldo(self):
+        print(f"Titular: {self.titular} | Saldo atual: R${self.saldo:.2f}")
+
+
+conta1 = ContaBancaria("Matheus", 1000)
+
+conta1.exibir_saldo()
+conta1.depositar(500)
+conta1.exibir_saldo()
--- teste.py
+++
@@ -1,4 +0,0 @@
-def funcao_teste():
-    print(
-        "isso aqui é um teste só para ver se o black vai quebrar essa linha gigante automaticamente"
-    )
