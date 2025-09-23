09d32b6fd20cf2f3835ac26f95a0505aaffa2902
BeiruthDEV <matheusbeiruth10@gmail.com>
2025-09-23 20:41:58 -0300

    Exercício 2 nivel Básico

---
+++ modulo 2/POO/exerc2_nvlbasico.py
@@ -0,0 +1,10 @@
+from exerc1_nvlbasico import Pessoa
+
+
+
+p1 = Pessoa("João", 25)
+p2 = Pessoa("Maria", 30)
+
+
+print(f"Nome: {p1.nome}, Idade: {p1.idade}")
+print(f"Nome: {p2.nome}, Idade: {p2.idade}")
