3b8489cb2318a4c4564667f3c777140e56c9bc84
BeiruthDEV <135391346+BeiruthDEV@users.noreply.github.com>
2025-11-12 10:25:58 -0300

    Aula 12/11

--- modulo_4/core/views.py
+++ modulo_4/core/views.py
@@ -1,19 +1,26 @@
 
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from .models import Tarefa
+from .forms import TarefaForm
 
-# Create your views here.
 def home(request):
-    todas_as_tarefas = Tarefa.objects.all()
-
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TarefaForm() 
+    todas_as_tarefas = Tarefa.objects.all().order_by('-criada_em') # Ordena pelas mais novas
     context = {
         'nome_usuario': 'Júnior',
-        'tecnologias': ['Python', 'Django', 'HTML', 'CSS'],
+        'tecnologias': ['Python', 'Django', 'Models', 'Forms'],
         'tarefas': todas_as_tarefas,
+        'form': form, 
     }
-    # return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
     return render(request, 'home.html', context)
+
 
 def login(request):
     return HttpResponse("<input>Login</input>")
